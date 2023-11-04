69e2677c344ba5c64dacb33202cb3a95ccc0c5b9
Vinicius Botelho <vbawada@gmail.com>
2023-11-03 23:37:50 -0300

    Add project files.

---
+++ Api.MasterChef/Models/RecipeDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Api.MasterChef.Models
+{
+    public class RecipeDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public List<string> Ingredients { get; set; } = new List<string>();
+        public List<string> Instructions { get; set; } = new List<string>();
+    }
+}
