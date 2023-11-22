a3f050088e06d671800482d894c8636262dc811c
Elias Oliveira <elias.oliveira@csprime.com.br>
2023-11-22 15:19:10 -0300

    subida de dropdownlist

--- Web.MasterChef/Controllers/RecipeController.cs
+++ Web.MasterChef/Controllers/RecipeController.cs
@@ -4,31 +4,52 @@ using MasterChef.Web.Utils;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Caching.Memory;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MasterChef.Web.Controllers
 {
 	public class RecipeController : Controller
 	{
 		private readonly IRecipeService _recipeService;
+		private readonly ICategoryService _categoryService;
+		private readonly IIngredientService _ingredientService;
+		private readonly IMemoryCache _cache;
 
-		public RecipeController(IRecipeService recipeService)
+
+
+		public RecipeController(IRecipeService recipeService, ICategoryService categoryService, IIngredientService ingredientService)
 		{
 			_recipeService = recipeService ?? throw new ArgumentNullException(nameof(recipeService));
+			_categoryService = categoryService ?? throw new ArgumentNullException(nameof(categoryService));
+			_ingredientService = ingredientService ?? throw new ArgumentNullException(nameof(ingredientService));
 		}
 		public async Task<IActionResult> RecipeIndex()
 		{
+
 			var recipes = await _recipeService.FindAllRecipes();
 			return View(recipes);
 		}
+		private async Task<ActionResult> popularCategoriaAsync()
+		{
 
+			ViewBag.Lista = await _categoryService.FindAllCategories();
+
+			return View();
+		}
 		public async Task<IActionResult> RecipeCreate()
 		{
+			ViewBag.CategoryId = new SelectList(	await _categoryService.FindAllCategories(),
+				"Id",
+				"Name"
+				);
 			return View();
 		}
 
-        //[Authorize]
-        [HttpPost]
-		public async Task<IActionResult> RecipeCreate(RecipeModel model)
+		//[Authorize]
+		[HttpPost]
+		public async Task<IActionResult> RecipeCreate(RecipeModel model, string CategoryId)
 		{
 			if (ModelState.IsValid)
 			{
@@ -38,15 +59,15 @@ namespace MasterChef.Web.Controllers
 			}
 			return View(model);
 		}
-        public async Task<IActionResult> RecipeUpdate(int id)
+		public async Task<IActionResult> RecipeUpdate(int id)
 		{
 			var model = await _recipeService.FindRecipeById(id);
 			if (model != null) return View(model);
 			return NotFound();
 		}
 
-        //[Authorize]
-        [HttpPost]
+		//[Authorize]
+		[HttpPost]
 		public async Task<IActionResult> RecipeUpdate(RecipeModel model)
 		{
 			if (ModelState.IsValid)
@@ -58,15 +79,15 @@ namespace MasterChef.Web.Controllers
 			return View(model);
 		}
 
-        //[Authorize]
-        public async Task<IActionResult> RecipeDelete(int id)
+		//[Authorize]
+		public async Task<IActionResult> RecipeDelete(int id)
 		{
 			var model = await _recipeService.FindRecipeById(id);
 			if (model != null) return View(model);
 			return NotFound();
 		}
 
-        [HttpPost]
+		[HttpPost]
 		//[Authorize(Roles = Role.Admin)]
 		public async Task<IActionResult> RecipeDelete(RecipeModel model)
 		{
--- Web.MasterChef/Models/RecipeModel.cs
+++ Web.MasterChef/Models/RecipeModel.cs
@@ -1,9 +1,10 @@
 ﻿namespace MasterChef.Web.Models
 {
-    public class RecipeModel
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
+	public class RecipeModel
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public CategoryModel Category { get; set; }
+		public string Description { get; set; }
+	}
 }
